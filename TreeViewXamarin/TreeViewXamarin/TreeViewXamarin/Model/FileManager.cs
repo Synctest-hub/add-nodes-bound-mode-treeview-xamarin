a5d87e50050f08aae679cb1ad8e356d565d1e9ed
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-30 11:10:17 +0530

    XAMARIN-34260 - Sample added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Model/FileManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class FileManager : INotifyPropertyChanged
+    {
+        private string itemName;
+        private ImageSource imageIcon;
+        private ObservableCollection<FileManager> subFiles;
+        public FileManager()
+        {
+
+        }
+        public ObservableCollection<FileManager> SubFiles
+        {
+            get { return subFiles; }
+            set
+            {
+                subFiles = value;
+                RaisedOnPropertyChanged("SubFiles");
+            }
+        }
+        public string ItemName
+        {
+            get { return itemName; }
+            set
+            {
+                itemName = value;
+                RaisedOnPropertyChanged("ItemName");
+            }
+        }
+        public ImageSource ImageIcon
+        {
+            get { return imageIcon; }
+            set
+            {
+                imageIcon = value;
+                RaisedOnPropertyChanged("ImageIcon");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+    }
+}
---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/ViewModel/FileManagerViewModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class FileManagerViewModel
+    {
+        public Command OnAddItem { get; set; }
+        private ObservableCollection<FileManager> imageNodeInfo;
+        public FileManagerViewModel()
+        {
+            GenerateSource();
+            OnAddItem =new Command(OnAddItemMethod);
+        }
+        private void OnAddItemMethod(object obj)
+        {
+            Assembly assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            var game = new FileManager() { ItemName = "Game", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_folder.png", assembly) };
+            var games = new FileManager() { ItemName = "Pubg.exe", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_exe.png", assembly) };
+            game.SubFiles =  new ObservableCollection<FileManager>
+            {
+                games
+            };
+            ImageNodeInfo.Add(game);
+        }
+        public ObservableCollection<FileManager> ImageNodeInfo
+        {
+            get { return imageNodeInfo; }
+            set { this.imageNodeInfo = value; }
+        }
+        private void GenerateSource()
+        {
+            var nodeImageInfo = new ObservableCollection<FileManager>();
+            Assembly assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            var doc = new FileManager() { ItemName = "Documents", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_folder.png", assembly) };
+            var download = new FileManager() { ItemName = "Downloads", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_folder.png", assembly) };
+            var mp3 = new FileManager() { ItemName = "Music", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_folder.png", assembly) };
+            var pictures = new FileManager() { ItemName = "Pictures", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_folder.png", assembly) };
+            var video = new FileManager() { ItemName = "Videos", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_folder.png", assembly) };
+
+            var pollution = new FileManager() { ItemName = "Environmental Pollution.docx", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_word.png", assembly) };
+            var globalWarming = new FileManager() { ItemName = "Global Warming.ppt", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_ppt.png", assembly) };
+            var sanitation = new FileManager() { ItemName = "Sanitation.docx", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_word.png", assembly) };
+            var socialNetwork = new FileManager() { ItemName = "Social Network.pdf", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_pdf.png", assembly) };
+            var youthEmpower = new FileManager() { ItemName = "Youth Empowerment.pdf", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_pdf.png", assembly) };
+
+            var games = new FileManager() { ItemName = "Game.exe", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_exe.png", assembly) };
+            var tutorials = new FileManager() { ItemName = "Tutorials.zip", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_zip.png", assembly) };
+            var typeScript = new FileManager() { ItemName = "TypeScript.7z", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_zip.png", assembly) };
+            var uiGuide = new FileManager() { ItemName = "UI-Guide.pdf", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_pdf.png", assembly) };
+
+            var song = new FileManager() { ItemName = "Gouttes", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_mp3.png", assembly) };
+
+            var camera = new FileManager() { ItemName = "Camera Roll", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_folder.png", assembly) };
+            var stone = new FileManager() { ItemName = "Stone.jpg", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_png.png", assembly) };
+            var wind = new FileManager() { ItemName = "Wind.jpg", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_png.png", assembly) };
+
+            var img0 = new FileManager() { ItemName = "WIN_20160726_094117.JPG", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_img0.png", assembly) };
+            var img1 = new FileManager() { ItemName = "WIN_20160726_094118.JPG", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_img1.png", assembly) };
+
+            var video1 = new FileManager() { ItemName = "Naturals.mp4", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_video.png", assembly) };
+            var video2 = new FileManager() { ItemName = "Wild.mpeg", ImageIcon = ImageSource.FromResource("TreeViewXamarin.Icons.treeview_video.png", assembly) };
+
+            doc.SubFiles = new ObservableCollection<FileManager>
+            {
+                pollution,
+                globalWarming,
+                sanitation,
+                socialNetwork,
+                youthEmpower
+            };
+
+            download.SubFiles = new ObservableCollection<FileManager>
+            {
+                games,
+                tutorials,
+                typeScript,
+                uiGuide
+            };
+
+            mp3.SubFiles = new ObservableCollection<FileManager>
+            {
+                song
+            };
+
+            pictures.SubFiles = new ObservableCollection<FileManager>
+            {
+                camera,
+                stone,
+                wind
+            };
+            camera.SubFiles = new ObservableCollection<FileManager>
+            {
+                img0,
+                img1
+            };
+
+            video.SubFiles = new ObservableCollection<FileManager>
+            {
+                video1,
+                video2
+            };
+
+            nodeImageInfo.Add(doc);
+            nodeImageInfo.Add(download);
+            nodeImageInfo.Add(mp3);
+            nodeImageInfo.Add(pictures);
+            nodeImageInfo.Add(video);
+            imageNodeInfo = nodeImageInfo;
+            ImageNodeInfo.CollectionChanged += OnCollectionChanged;
+        }
+        private void OnCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            App.Current.MainPage.DisplayAlert("Alert", "New Item added", "OK");
+        }
+    }
+}
